f7d40ba3e0a8e7b16af5b69a1cfe8ca513c210db
Jesus Politron <jesuspolitronrangel@gmail.com>
2026-07-18 13:54:53 -0600

    add: project basic structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
 import { lazy, Suspense } from "react";
 import { Routes, Route } from "react-router";
 
+const Home = lazy(() => import("./pages/home"));
+const Likes = lazy(() => import("./pages/likes"));
+const Error = lazy(() => import("./pages/error"));
+
 function App() {
-  return <></>;
+  return (
+    <Suspense fallback="Loading...">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/likes" element={<Likes />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </Suspense>
+  );
 }
 
 export default App;
