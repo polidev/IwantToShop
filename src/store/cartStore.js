cb80e07d32f902d563cad86502bad70b1459aa9b
Jesus Politron <jesuspolitronrangel@gmail.com>
2026-07-19 12:32:43 -0600

    add: persisting storage

--- src/store/cartStore.js
+++ src/store/cartStore.js
@@ -1,64 +1,79 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
-const useCartStore = create((set, get) => ({
-  // initial state
-  items: [],
+const useCartStore = create(
+  persist(
+    (set, get) => ({
+      // initial state
+      items: [],
 
-  // actions
-  clearCart: () => set({ items: [] }),
+      // actions
+      clearCart: () => set({ items: [] }),
 
-  // add to cart
-  addItem: (product) => {
-    set((state) => {
-      const existingItem = state.items.find((item) => item.id === product.id);
+      // add to cart
+      addItem: (product) => {
+        set((state) => {
+          const existingItem = state.items.find(
+            (item) => item.id === product.id,
+          );
 
-      if (existingItem) {
-        return {
-          items: state.items.map((item) =>
-            item.id === product.id
-              ? { ...item, quantity: item.quantity + 1 }
-              : item,
-          ),
-        };
-      }
+          if (existingItem) {
+            return {
+              items: state.items.map((item) =>
+                item.id === product.id
+                  ? { ...item, quantity: item.quantity + 1 }
+                  : item,
+              ),
+            };
+          }
 
-      return {
-        items: [...state.items, { ...product, quantity: 1 }],
-      };
-    });
-  },
+          return {
+            items: [...state.items, { ...product, quantity: 1 }],
+          };
+        });
+      },
 
-  // remove from cart
-  removeItem: (productId) => {
-    set((state) => {
-      return {
-        items: state.items.filter((item) => item.id !== productId),
-      };
-    });
-  },
+      // remove from cart
+      removeItem: (productId) => {
+        set((state) => {
+          return {
+            items: state.items.filter((item) => item.id !== productId),
+          };
+        });
+      },
 
-  // update quantity
-  updateQuantity: (productId, quantity) => {
-    set((state) => {
-      return {
-        items: state.items.map((item) =>
-          item.id === productId ? { ...item, quantity } : item,
-        ),
-      };
-    });
-  },
+      // update quantity
+      updateQuantity: (productId, quantity) => {
+        set((state) => {
+          return {
+            items: state.items
+              .map((item) =>
+                item.id === productId ? { ...item, quantity } : item,
+              )
+              .filter((item) => item.quantity > 0), // remove items with quantity  hints 0
+          };
+        });
+      },
 
-  // total price
-  getTotalPrice: () => {
-    const items = get().items;
-    return items.reduce((total, item) => total + item.price * item.quantity, 0);
-  },
+      // total price
+      getTotalPrice: () => {
+        const items = get().items;
+        return items.reduce(
+          (total, item) => total + item.price * item.quantity,
+          0,
+        );
+      },
 
-  // total quantity
-  getTotalQuantity: () => {
-    const items = get().items;
-    return items.reduce((total, item) => total + item.quantity, 0);
-  },
-}));
+      // total quantity
+      getTotalQuantity: () => {
+        const items = get().items;
+        return items.reduce((total, item) => total + item.quantity, 0);
+      },
+    }),
+    {
+      name: "cart-storage", // key for localStorage
+    },
+  ),
+);
 
 export default useCartStore;
