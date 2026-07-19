3d5758fc760a91746062edf7086531a8dd057803
Jesus Politron <jesuspolitronrangel@gmail.com>
2026-07-19 12:57:02 -0600

    fix: zustand const import

--- src/components/layout/header.jsx
+++ src/components/layout/header.jsx
@@ -1,7 +1,26 @@
+import useCartStore from "../../store/cartStore.js";
+
 export default function Header() {
+  const totalQuantity = useCartStore((state) => state.getTotalQuantity());
+
   return (
     <header>
-      <h1>Header</h1>
+      <nav>
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            {totalQuantity >= 0 && (
+              <span className="cart-quantity">{totalQuantity}</span>
+            )}
+          </li>
+
+          <li>
+            <a href="/cart">Cart</a>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 }
